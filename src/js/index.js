8e8a46325eec2240d697d2b5d7d616be80749de2
hrbraga <hugbraga@gmail.com>
2024-09-18 11:36:28 -0300

    Rodapá da página

---
+++ src/js/index.js
@@ -0,0 +1,16 @@
+window.onscroll = function() {
+    const button = document.getElementById('backToTop');
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        button.style.display = 'block';
+    } else {
+        button.style.display = 'none';
+    }
+};
+
+// Função para voltar ao topo
+document.getElementById('backToTop').onclick = function() {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth' // Animação suave
+    });
+};
